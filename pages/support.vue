3758e8514c9d51a06c3424fd0e27b23b89fa36f8
Eulo Rod Coting <eulorodcoting@gmail.com>
2026-03-10 13:57:20 +0800

    [feat] Implement Google Fonts configuration

--- pages/support.vue
+++ pages/support.vue
@@ -11,16 +11,11 @@ useSeoMeta({
 useHead({
  link: [
  {rel: 'icon', type: 'image/png', href: '/logo.png'},
- {rel: 'stylesheet', href: '/reset.css'}
- ]
-});</script>
-<template>
- <client-only>
-<NavBar />
- <Welcome />
- <div>This is support page</div>
-<Copyright />
- </client-only>
-</template>
+ {rel: 'stylesheet', href: '/reset.css'},
+ {rel: 'stylesheet', href: '/custom.css'} ,
+ {rel: 'preconnect', href: 'https://fonts.googleapis.com'},
+ {rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: ''},
+ {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Geom:ital,wght@0,300..900;1,300..900&display=swap'}
+ ]});</script>
 <style scoped></style>
 <style></style>
