7c126873ef2dcc24670c84dc01d8efad869c26b0
Eulo Rod Coting <eulorodcoting@gmail.com>
2026-03-11 22:45:31 +0800

    [UI] Add NavBar component with design and usage

---
+++ components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+ <client-only>
+ <header class="bt-navbar">
+ <nav class="bt-nav-left">
+ <a href="/" class="bt-nav-home">
+ <img src="/logo.png" width=20 height=20 class="bt-nav-logo">
+ <span>Bleedingheart</span>
+ </a>
+ </nav>
+ <nav class="bt-nav-right">
+ <ul>
+ <li><a href="#" class="bt-nav-link">Upload Artwork</a></li>
+ <li><a href="#" class="bt-nav-link">View Artwork Feed</a></li>
+ <li><a href="#" class="bt-nav-link">Edit Profile</a></li>
+ </ul>
+ </nav>
+ </header>
+ </client-only>
+</template>
--- pages/terms.vue
+++ pages/terms.vue
@@ -1,13 +1,23 @@
+<script lang="ts"></script>
+<template>
+ <client-only>
+ <!-- <NavBar /> -->
+ <div id="btMain" class="bt-main"></div>
+ <!-- <Cookies /> -->
+ <!-- <Copyright /> -->
+ </client-only>
+</template>
 <script setup lang="ts">
 import { useSeoMeta, useHead } from '@vueuse/head';
 const title = "Baby Turon | Home";
-const description = "Easier life!";
+const description = "A media platform for digital artists.";
 useSeoMeta({
  title: () => title,
  description: () => description,
  charset: "utf-8",
  viewport: "width=device-width, initial-scale=1.0"
 });
+
 useHead({
  link: [
  {rel: 'icon', type: 'image/png', href: '/logo.png'},
@@ -16,6 +26,9 @@ useHead({
  {rel: 'preconnect', href: 'https://fonts.googleapis.com'},
  {rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: ''},
  {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Geom:ital,wght@0,300..900;1,300..900&display=swap'}
- ]});</script>
+ ]});
+ </script>
+
 <style scoped></style>
-<style></style>
+
+<style></style> 
